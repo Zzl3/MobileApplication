8c8ab59e607d48de9441384be0a4f5c5c86d3ef1
sihhd <109146234+sihhdgyh@users.noreply.github.com>
2023-05-19 16:29:36 +0800

    1

--- Music/Music/Style_Com/InstruTab.swift
+++ Music/Music/Style_Com/InstruTab.swift
@@ -16,7 +16,7 @@ struct InstruTab:Identifiable{
 
 var tabs:[InstruTab]=[
     .init(tabImage:"Instru_guzheng",tabName:"古筝",tabOffset:CGSize(width: 0, height: -40)),
-    .init(tabImage:"Instru_muyu",tabName:"木鱼",tabOffset:CGSize(width: 0, height: -38)),
-    .init(tabImage:"Instru_yangqin",tabName:"扬琴",tabOffset:CGSize(width: 0, height: -25)),
-    .init(tabImage:"Instru_sheng",tabName:"笙",tabOffset:CGSize(width: -12, height: 28)),
+    .init(tabImage:"Instru_xiao",tabName:"萧",tabOffset:CGSize(width: 0, height: -38)),
+    .init(tabImage:"Instru_di",tabName:"笛子",tabOffset:CGSize(width: 0, height: -25)),
+    .init(tabImage:"",tabName:"",tabOffset:CGSize(width: -12, height: 28)),
 ]
